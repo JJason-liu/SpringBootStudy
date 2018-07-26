6b89ef4ca1e89a9a77b56232f100b42f90d05cd2
JJason-liu <765798166@qq.com>
2018-07-26 17:50:50 +0800

    首个实例

--- src/main/java/com/example/hellowordemo/SpringbootdemoApplication.java
+++ src/main/java/com/example/hellowordemo/SpringbootdemoApplication.java
@@ -1,10 +1,18 @@
-package com.example.springbootdemo;
+package com.example.hellowordemo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class SpringbootdemoApplication {
+
+	@RequestMapping("/")
+	String home(){
+		return "Hello word!!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
